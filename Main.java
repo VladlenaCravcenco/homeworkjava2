cea9dea5ba56ef771d820b650126d2637868346a
VladlenaCravcenco <61877933+VladlenaCravcenco@users.noreply.github.com>
2021-09-30 10:53:07 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,28 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int number1;
+        int number2;
+
+        System.out.print("Enter first integer: ");
+        number1 = input.nextInt();
+
+        System.out.print("Enter first integer: ");
+        number2 = input.nextInt();
+
+        if (number1 == number2)
+            System.out.println("Numbers are equal");
+
+        if (number1 > number2)
+            System.out.printf("%d is greater",number1);
+
+        if (number1 < number2)
+            System.out.printf("%d is greater",number2);
+    }
+}
